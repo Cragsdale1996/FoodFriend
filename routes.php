95d7cc0e78a4fa2134cf80cd67f15c8b8e956c22
Nhi Dang <dangnhi169@gmail.com>
2017-04-19 16:19:30 -0500

    homepage (top 5 dishes)

---
+++ routes.php
@@ -0,0 +1,22 @@
+<?php
+
+// Retrieve todo with id
+$app->get('/home', function ($request, $response, $args) {
+  mysqli_set_charset("utf8");
+    $sth = $this->db->prepare("SELECT RestAccount.name as Restaurant_name, dish.name as 'Dish_name', description, score
+      FROM (SELECT rest_id, Dishes.name, description, score FROM Dishes ORDER BY score DESC LIMIT 5) AS dish
+      JOIN RestAccount
+      ON RestAccount.rest_id = dish.rest_id");
+
+      $sth->execute();
+      $list = $sth->fetchAll();
+      return $this->response->withJson($list);
+});
+
+$app->get('/[{name}]', function ($request, $response, $args) {
+    // Sample log message
+    $this->logger->info("Slim-Skeleton '/' route");
+
+    // Render index view
+    return $this->renderer->render($response, 'index.phtml', $args);
+});
