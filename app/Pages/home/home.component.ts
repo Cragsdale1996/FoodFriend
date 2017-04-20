6f6b79ab299a421d9d6446870abc351b6dac6ea4
B0bTh3BuiIder <rbrennanpick@smu.edu>
2017-04-19 20:10:58 -0500

    Includes Home page
    Page not actually done. Just exists now

---
+++ app/Pages/home/home.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule } from '@angular/forms';
+import { RouterModule, Router, ActivatedRoute } from '@angular/router';
+
+@Component({
+	moduleId: module.id,
+  selector: 'home',
+  templateUrl: 'home.component.html',
+  styleUrls: [ 'home.component.css' ]
+})
+
+export class HomeComponent {
+
+
+}
