a4b8224d0dbaef27d5ac2f1dc56e28cfa7f8f502
Ubuntu <ubuntu@ip-172-31-45-58.us-west-2.compute.internal>
2017-05-02 00:27:39 +0000

    edited endpoints

--- src/pageFiles/homepage.php
+++ src/pageFiles/homepage.php
@@ -1,5 +1,4 @@
 <?php
-
 // Homepage: Display top 5 dishes
 $app->get('/home', function ($request, $response, $args) {
     $sth = $this->db->prepare("SELECT RestAccount.name as Restaurant_name, dish.name as 'Dish_name', description, score
@@ -8,5 +7,5 @@ $app->get('/home', function ($request, $response, $args) {
       ON RestAccount.rest_id = dish.rest_id");
       $sth->execute();
       $list = $sth->fetchAll();
-      return $this->response->withJson($list);
+      return $this->response->withAddedHeader('Access-Control-Allow-Origin', '*')->withJson($list);
 });
